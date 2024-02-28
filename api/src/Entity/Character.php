6d284d8f9b501fe0417144e0a1cebaf29651b80d
NimZero <contact@nimzero.fr>
2024-02-28 12:04:00 +0100

    Start entities

---
+++ api/src/Entity/Character.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\CharacterRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+#[ApiResource()]
+#[Get()]
+#[GetCollection()]
+#[Post(security: "is_granted('ROLE_ADMIN')")]
+#[Put(security: "is_granted('ROLE_ADMIN')")]
+#[Delete(security: "is_granted('ROLE_ADMIN')")]
+#[Patch(security: "is_granted('ROLE_ADMIN')")]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $firstname = null;
+
+    #[ORM\Column]
+    private ?int $height = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $birthdate = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $hairColor = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $astrologicalSign = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $programmingLanguage = null;
+
+    #[ORM\OneToMany(mappedBy: 'character', targetEntity: Anectode::class, orphanRemoval: true)]
+    private Collection $anectodes;
+
+    #[ORM\OneToMany(mappedBy: 'character', targetEntity: Image::class, orphanRemoval: true)]
+    private Collection $images;
+
+    public function __construct()
+    {
+        $this->anectodes = new ArrayCollection();
+        $this->images = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): static
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeImmutable
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(\DateTimeImmutable $birthdate): static
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    public function getHairColor(): ?string
+    {
+        return $this->hairColor;
+    }
+
+    public function setHairColor(string $hairColor): static
+    {
+        $this->hairColor = $hairColor;
+
+        return $this;
+    }
+
+    public function getAstrologicalSign(): ?string
+    {
+        return $this->astrologicalSign;
+    }
+
+    public function setAstrologicalSign(string $astrologicalSign): static
+    {
+        $this->astrologicalSign = $astrologicalSign;
+
+        return $this;
+    }
+
+    public function getProgrammingLanguage(): ?string
+    {
+        return $this->programmingLanguage;
+    }
+
+    public function setProgrammingLanguage(string $programmingLanguage): static
+    {
+        $this->programmingLanguage = $programmingLanguage;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Anectode>
+     */
+    public function getAnectodes(): Collection
+    {
+        return $this->anectodes;
+    }
+
+    public function addAnectode(Anectode $anectode): static
+    {
+        if (!$this->anectodes->contains($anectode)) {
+            $this->anectodes->add($anectode);
+            $anectode->setCharacter($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnectode(Anectode $anectode): static
+    {
+        if ($this->anectodes->removeElement($anectode)) {
+            // set the owning side to null (unless already changed)
+            if ($anectode->getCharacter() === $this) {
+                $anectode->setCharacter(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): static
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setCharacter($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): static
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getCharacter() === $this) {
+                $image->setCharacter(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Image.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+#[ApiResource()]
+#[Get()]
+#[GetCollection()]
+#[Post(security: "is_granted('ROLE_ADMIN')")]
+#[Put(security: "is_granted('ROLE_ADMIN')")]
+#[Delete(security: "is_granted('ROLE_ADMIN')")]
+#[Patch(security: "is_granted('ROLE_ADMIN')")]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'images')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Character $character = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $link = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $path = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCharacter(): ?Character
+    {
+        return $this->character;
+    }
+
+    public function setCharacter(?Character $character): static
+    {
+        $this->character = $character;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): static
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): static
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
