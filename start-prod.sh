515117e5cfd762111a8a23651deff6442f0efc9e
NimZero <contact@nimzero.fr>
2024-04-30 09:37:56 +0200

    Update deps, change docker config

--- start-prod.sh
+++ start-prod.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SERVER_NAME=http://epsidle.nimzero.fr \
-HTTP_PORT=15012 \
-HTTP3_PORT=15012 \
+SERVER_NAME=epsidle.nimzero.fr \
+HTTP_PORT=15021 \
+HTTPS_PORT=15022 \
+HTTP3_PORT=15022 \
 APP_SECRET=ChangeMe \
 CADDY_MERCURE_JWT_SECRET=ChangeThisMercureHubJWTSecretKey \
 docker compose -f compose.yaml -f compose.prod.yaml up -d --wait
